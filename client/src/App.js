777740957b29498170b998849764fadbbc9addba
Ryland Adams <rylandadams@yahoo.com>
2022-12-31 14:43:29 -0800

    listing out posts from DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
-	return <div className='App'></div>;
+	const [listOfPosts, setListOfPosts] = useState([]);
+
+	useEffect(() => {
+		axios.get('http://localhost:3001/posts').then((response) => {
+			setListOfPosts(response.data);
+		});
+	}, []);
+
+	return (
+		<div className='App'>
+			{' '}
+			{listOfPosts.map((value, key) => {
+				return (
+					<div className='post'>
+						<div className='title'> {value.title} </div>
+						<div className='body'> {value.postText} </div>
+						<div className='footer'> {value.username} </div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
