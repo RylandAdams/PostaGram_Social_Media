7c9a15653acef3d95a7724071f1ac4d00a95040f
Ryland Adams <rylandadams@yahoo.com>
2022-12-31 12:53:42 -0800

    Post and Get for Posts table working!

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const db = require('./models');
+
+// Routers
+const postRouter = require('./routes/Posts');
+app.use('/posts', postRouter);
 
 db.sequelize.sync().then(() => {
 	app.listen(3001, () => {
