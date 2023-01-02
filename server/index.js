250ae0c15f5285fcacff42aefcd5298884b33208
Ryland Adams <rylandadams@yahoo.com>
2023-01-02 10:49:06 -0800

    server and SQL creating comments and typing it to posts table

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ const db = require('./models');
 const postRouter = require('./routes/Posts');
 app.use('/posts', postRouter);
 
+const commentsRouter = require('./routes/Comments');
+app.use('/comments', commentsRouter);
+
 db.sequelize.sync().then(() => {
 	app.listen(3001, () => {
 		console.log('Server running on port 3001');
--- server/models/Posts.js
+++ server/models/Posts.js
@@ -14,5 +14,11 @@ module.exports = (sequelize, DataTypes) => {
 		},
 	});
 
+	Posts.associate = (models) => {
+		Posts.hasMany(models.Comments, {
+			onDelete: 'cascade',
+		});
+	};
+
 	return Posts;
 };
