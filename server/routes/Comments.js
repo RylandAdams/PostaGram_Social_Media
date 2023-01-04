501327fb8229046c22df0d420fd05677c7b7ef03
Ryland Adams <rylandadams@yahoo.com>
2023-01-04 10:56:19 -0800

    Auth for comment posting

--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { Comments } = require('../models');
 
+const { validateToken } = require('../middlewares/AuthMiddleware');
+
 router.get('/:postId', async (req, res) => {
 	const postId = req.params.postId;
 	const comments = await Comments.findAll({ where: { PostId: postId } });
 	res.json(comments);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', validateToken, async (req, res) => {
 	const comment = req.body;
 	await Comments.create(comment);
 	res.json(comment);
