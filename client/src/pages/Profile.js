9b04283abb65917831dad27f5fd767dbb2174723
Ryland Adams <rylandadams@yahoo.com>
2023-01-09 16:38:03 -0800

    added in profile page that gives/shows all data realted to user

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function Profile() {
 	let { id } = useParams();
 	const [username, setUsername] = useState('');
+	const [listOfPosts, setListOfPosts] = useState([]);
+
+	let navigate = useNavigate();
 
 	useEffect(() => {
 		axios
@@ -12,13 +16,48 @@ function Profile() {
 			.then((response) => {
 				setUsername(response.data.username);
 			});
+
+		axios
+			.get(`http://localhost:3001/posts/byUserId/${id}`)
+			.then((response) => {
+				setListOfPosts(response.data);
+			});
 	}, []);
+
 	return (
 		<div className='profilePageContainer'>
 			<div className='basicInfo'>
 				<h1>Username: {username}</h1>
 			</div>
-			<div className='listOfPosts'></div>
+			<div className='listOfPosts'>
+				{listOfPosts.map((value, key) => {
+					return (
+						<div
+							key={key}
+							className='post'
+						>
+							<div className='title'> {value.title} </div>
+							<div
+								className='body'
+								onClick={() => {
+									navigate(`/post/${value.id}`);
+								}}
+							>
+								{value.postText}
+							</div>
+							<div className='footer'>
+								<div className='username'>
+									{' '}
+									{value.username}{' '}
+								</div>
+								<div className='buttons'>
+									<label>{value.Likes.length}</label>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
